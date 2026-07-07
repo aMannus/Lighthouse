6348bd247ce18679659910ab249dc2b068e652c4
aMannus <mannusmenting@gmail.com>
2026-07-07 21:12:28 +0200

    Not working pulling functions from map

--- src/port/Rando/CustomCollectible/CustomCollectible.cpp
+++ src/port/Rando/CustomCollectible/CustomCollectible.cpp
@@ -1,5 +1,6 @@
 #include "CustomCollectible.h"
 #include "actor.h"
+#include <unordered_map>
 
 extern "C" {
 Actor* actor_new(s32 position[3], s32 yaw, ActorInfo* actorInfo, u32 flags);
@@ -8,14 +9,20 @@ Actor* marker_getActor(ActorMarker* thisx);
 void marker_despawn(ActorMarker* marker);
 }
 
+using DrawFunc = Actor* (*)(ActorMarker*, Gfx**, Mtx**, Vtx**);
+
+std::unordered_map<RandoItemId, CustomCollecticleDrawData> customCollectibleDrawList = {
+    { RI_EMPTY_HONEYCOMB_BUBBLEGLOOP_SWAMP, { ASSET_35F_MODEL_JIGGY, CustomCollectible_Draw } },
+};
+
 ActorInfo customActorInfo = { MARKER_300_CUSTOM_COLLECTIBLE,
                               ACTOR_3CD_CUSTOM_COLLECTIBLE,
-                              ASSET_35F_MODEL_JIGGY,
+                              ASSET_0_NONE,
                               1,
                               NULL,
-                              CustomCollectible_Update,
                               NULL,
-                              (Actor*(*)(ActorMarker*, Gfx**, Mtx**, Vtx**)) CustomCollectible_Draw,
+                              NULL,
+                              NULL,
                               0,
                               0,
                               0.9f,
@@ -32,9 +39,10 @@ void CustomCollectible_Update(Actor* actor) {
     actor->yaw += 5.0f;
 }
 
-void CustomCollectible_Draw(ActorMarker* marker, Gfx** gdl, Mtx** mptr, Vtx** arg3) {
+Actor* CustomCollectible_Draw(ActorMarker* marker, Gfx** gdl, Mtx** mptr, Vtx** arg3) {
     Actor* actor = marker_getActor(marker);
     actor = actor_draw(actor->marker, gdl, mptr, arg3);
+    return actor;
 }
 
 Actor* CustomCollectible::AttachLocalStruct(int32_t randoItemId, Actor* customCollectible) {
@@ -50,9 +58,14 @@ Actor* CustomCollectible::Spawn(int32_t position[3], RandoItemId randoItemId) {
 
     int32_t flags = ACTOR_FLAG_UNKNOWN_6 | ACTOR_FLAG_UNKNOWN_7 | ACTOR_FLAG_UNKNOWN_21;
 
+    auto drawData = customCollectibleDrawList.find(randoItemId);
+    ActorInfo collectibleInfo = customActorInfo;
+    collectibleInfo.modelId = drawData->second.drawModel;
+    collectibleInfo.draw_func = drawData->second.drawFunction;
+
     Actor* customCollectible = actor_new(position, 0, &customActorInfo, flags);
     customCollectible->marker->collisionFunc = CustomCollectible::OnCollect;
-    customCollectible = CustomCollectible::AttachLocalStruct(RI_EMPTY_HONEYCOMB_BUBBLEGLOOP_SWAMP, customCollectible);
+    customCollectible = CustomCollectible::AttachLocalStruct(randoItemId, customCollectible);
 
     return customCollectible;
 }
--- src/port/Rando/CustomCollectible/CustomCollectible.h
+++ src/port/Rando/CustomCollectible/CustomCollectible.h
@@ -1,13 +1,19 @@
 #pragma once
 #include "prop.h"
 #include "port/Rando/Types.h"
+#include <functional>
 
 typedef struct {
     uint32_t randoItemId;
 } ActorLocal_CustomCollectible;
 
+typedef struct {
+    asset_e drawModel;
+    std::function<Actor*(ActorMarker* marker, Gfx** gdl, Mtx** mptr, Vtx** arg3)> drawFunction;
+} CustomCollecticleDrawData;
+
 void CustomCollectible_Update(Actor* actor);
-void CustomCollectible_Draw(ActorMarker* marker, Gfx** gdl, Mtx** mptr, Vtx** arg3);
+Actor* CustomCollectible_Draw(ActorMarker* marker, Gfx** gdl, Mtx** mptr, Vtx** arg3);
 
 class CustomCollectible {
 private:
