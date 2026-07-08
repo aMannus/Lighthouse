1d085e2c432b21d60951fd5fb2f1d8a24249eecb
aMannus <mannusmenting@gmail.com>
2026-07-09 00:15:17 +0200

    Custom models working but update function doesn't always

--- src/port/Rando/CustomCollectible/CustomCollectible.cpp
+++ src/port/Rando/CustomCollectible/CustomCollectible.cpp
@@ -1,14 +1,34 @@
 #include "CustomCollectible.h"
+#include "CustomCollectibleDrawCustom.h"
 #include "actor.h"
 #include <unordered_map>
+#include "port/Rando/Rando.h"
+#include <print>
 
 extern "C" {
+#include "core1/sns.h"
 Actor* actor_new(s32 position[3], s32 yaw, ActorInfo* actorInfo, u32 flags);
 Actor* actor_draw(ActorMarker* marker, Gfx** gfx, Mtx** mtx, Vtx** vtx);
 Actor* fxTouchSparkle_draw(ActorMarker* marker, Gfx** gfx, Mtx** mtx, Vtx** vtx);
 Actor* marker_getActor(ActorMarker* thisx);
 void marker_despawn(ActorMarker* marker);
+Actor* func_802D41C4(ActorMarker* marker, Gfx** gfx, Mtx** mtx, Vtx** vtx);
 }
+
+std::unordered_map<actor_e, CustomCollectibleDrawInfo> customCollectibleDrawInfo = { 
+    { ACTOR_47_EMPTY_HONEYCOMB, { ASSET_361_MODEL_EMPTY_HONEYCOMB, CCT_VANILLA_MODEL } },
+    { ACTOR_46_JIGGY, { ASSET_35F_MODEL_JIGGY, CCT_VANILLA_MODEL } },
+    { ACTOR_60_JINJO_BLUE, { ASSET_3C0_MODEL_JINJO_BLUE, CCT_VANILLA_MODEL } },
+    { ACTOR_62_JINJO_GREEN, { ASSET_3C2_MODEL_JINJO_GREEN, CCT_VANILLA_MODEL } },
+    { ACTOR_5F_JINJO_ORANGE, { ASSET_3BC_MODEL_JINJO_ORANGE, CCT_VANILLA_MODEL } },
+    { ACTOR_61_JINJO_PINK, { ASSET_3C1_MODEL_JINJO_PINK, CCT_VANILLA_MODEL } },
+    { ACTOR_5E_JINJO_YELLOW, { ASSET_3BB_MODEL_JINJO_YELLOW, CCT_VANILLA_MODEL } },
+    { ACTOR_12C_MOLEHILL, { ASSET_388_MODEL_MOLEHILL, CCT_VANILLA_MODEL } },
+    { ACTOR_2D_MUMBO_TOKEN, { ASSET_41A_SPRITE_MUMBO_TOKEN, CCT_VANILLA_SPRITE } },
+    { ACTOR_51_MUSIC_NOTE, { ASSET_6D6_SPRITE_MUSIC_NOTE, CCT_VANILLA_SPRITE } },
+    { ACTOR_25E_SNS_EGG, { ASSET_50D_MODEL_SNS_EGG, CCT_VANILLA_SNS_EGG } },
+    { ACTOR_25D_ICE_KEY, { ASSET_0_NONE, CCT_CUSTOM_MODEL } },
+};
 
 ActorInfo customActorInfo = { MARKER_300_CUSTOM_COLLECTIBLE,
                               ACTOR_3CD_CUSTOM_COLLECTIBLE,
@@ -23,7 +43,7 @@ ActorInfo customActorInfo = { MARKER_300_CUSTOM_COLLECTIBLE,
                               0.9f,
                               0 };
 
-void CustomCollectible_Update(Actor* actor) {
+void Update(Actor* actor) {
     ActorLocal_CustomCollectible* customLocal = (ActorLocal_CustomCollectible*)&actor->local;
 
     if (!actor->initialized) {
@@ -32,18 +52,18 @@ void CustomCollectible_Update(Actor* actor) {
 
         if (customLocal->randoItemId >= RI_MUSIC_NOTE_BUBBLEGLOOP_SWAMP &&
             customLocal->randoItemId <= RI_MUSIC_NOTE_TREASURE_TROVE_COVE) {
-            actor->scale = 0.42857143f;
+            actor->scale = 0.4f;
+        } else if (customLocal->randoItemId >= RI_STOP_N_SWOP_EGG_BLUE &&
+                   customLocal->randoItemId <= RI_STOP_N_SWOP_EGG_YELLOW) {
+            actor->scale = 0.4f;
         }
     }
 
     actor->yaw += 5.0f;
 }
 
-Actor* CustomCollectible_DrawCustom(ActorMarker* marker, Gfx** gdl, Mtx** mptr, Vtx** vtx) {
-    Actor* actor = marker_getActor(marker);
-    actor = actor_draw(actor->marker, gdl, mptr, vtx);
-
-    return actor;
+void CustomCollectible_Update(Actor* actor) {
+    Update(actor);
 }
 
 Actor* CustomCollectible::AttachCustomVariables(int32_t randoItemId, Actor* customCollectible) {
@@ -58,118 +78,67 @@ Actor* CustomCollectible::Spawn(int32_t position[3], RandoItemId randoItemId) {
 
     int32_t flags = ACTOR_FLAG_UNKNOWN_6 | ACTOR_FLAG_UNKNOWN_7 | ACTOR_FLAG_UNKNOWN_21;
 
+    // Custom models exported for OoT are centered instead of offset to the marker, so they appear underground otherwise
+    actor_e actorId = (actor_e)Rando::StaticData::Items.find(randoItemId)->second.actorId;
+    auto drawInfo = customCollectibleDrawInfo.find(actorId);
+    if (drawInfo->second.drawType == CCT_CUSTOM_MODEL) {
+        position[1] += 50;
+    }
+
     ActorInfo collectibleInfo = CustomCollectible::GetActorAndDrawInfo(randoItemId);
 
     Actor* customCollectible = actor_new(position, 0, &collectibleInfo, flags);
+    if (Rando::StaticData::Items.find(randoItemId)->second.randoItemType == RITYPE_SNS_EGG) {
+        customCollectible->actorTypeSpecificField = CustomCollectible::GetSNSEggColor(randoItemId);
+    }
     customCollectible->marker->collisionFunc = CustomCollectible::OnCollect;
     customCollectible = CustomCollectible::AttachCustomVariables(randoItemId, customCollectible);
 
     return customCollectible;
 }
 
+uint32_t CustomCollectible::GetSNSEggColor(RandoItemId randoItemId) {
+    switch (randoItemId) { 
+        case RI_STOP_N_SWOP_EGG_BLUE:
+            return (uint32_t)SNS_ITEM_EGG_BLUE;
+        case RI_STOP_N_SWOP_EGG_CYAN:
+            return (uint32_t)SNS_ITEM_EGG_CYAN;
+        case RI_STOP_N_SWOP_EGG_GREEN:
+            return (uint32_t)SNS_ITEM_EGG_GREEN;
+        case RI_STOP_N_SWOP_EGG_PINK:
+            return (uint32_t)SNS_ITEM_EGG_PINK;
+        case RI_STOP_N_SWOP_EGG_RED:
+            return (uint32_t)SNS_ITEM_EGG_RED;
+        case RI_STOP_N_SWOP_EGG_YELLOW:
+            return (uint32_t)SNS_ITEM_EGG_YELLOW;
+        default:
+            return (uint32_t)SNS_ITEM_EGG_CYAN;
+    }
+}
+
 ActorInfo CustomCollectible::GetActorAndDrawInfo(RandoItemId randoItemId) {
     ActorInfo collectibleInfo = customActorInfo;
 
-    // Empty Honeycomb
-    if (randoItemId >= RI_EMPTY_HONEYCOMB_BUBBLEGLOOP_SWAMP && randoItemId <= RI_EMPTY_HONEYCOMB_TREASURE_TROVE_COVE) {
-        collectibleInfo.modelId = ASSET_363_MODEL_HONEYCOMB;
-        collectibleInfo.draw_func = actor_draw;
-    }
+    actor_e actorId = (actor_e)Rando::StaticData::Items.find(randoItemId)->second.actorId;
+    auto drawInfo = customCollectibleDrawInfo.find(actorId);
 
-    // Jiggy
-    else if (randoItemId >= RI_JIGGY_BUBBLEGLOOP_SWAMP && randoItemId <= RI_JIGGY_TREASURE_TROVE_COVE) {
-        collectibleInfo.modelId = ASSET_35F_MODEL_JIGGY;
-        collectibleInfo.draw_func = actor_draw;
-    }
+    collectibleInfo.modelId = drawInfo->second.drawModel;
 
-    // Jinjo (Blue)
-    else if (randoItemId >= RI_JINJO_BLUE_BUBBLEGLOOP_SWAMP && randoItemId <= RI_JINJO_BLUE_TREASURE_TROVE_COVE) {
-        collectibleInfo.modelId = ASSET_3C0_MODEL_JINJO_BLUE;
-        collectibleInfo.draw_func = actor_draw;
-    }
-
-    // Jinjo (Green)
-    else if (randoItemId >= RI_JINJO_GREEN_BUBBLEGLOOP_SWAMP && randoItemId <= RI_JINJO_GREEN_TREASURE_TROVE_COVE) {
-        collectibleInfo.modelId = ASSET_3C2_MODEL_JINJO_GREEN;
-        collectibleInfo.draw_func = actor_draw;
-    }
-
-    // Jinjo (Orange)
-    else if (randoItemId >= RI_JINJO_ORANGE_BUBBLEGLOOP_SWAMP && randoItemId <= RI_JINJO_ORANGE_TREASURE_TROVE_COVE) {
-        collectibleInfo.modelId = ASSET_3BC_MODEL_JINJO_ORANGE;
-        collectibleInfo.draw_func = actor_draw;
-    }
-
-    // Jinjo (Pink)
-    else if (randoItemId >= RI_JINJO_PINK_BUBBLEGLOOP_SWAMP && randoItemId <= RI_JINJO_PINK_TREASURE_TROVE_COVE) {
-        collectibleInfo.modelId = ASSET_3C1_MODEL_JINJO_PINK;
-        collectibleInfo.draw_func = actor_draw;
-    }
-
-    // Jinjo (Yellow)
-    else if (randoItemId >= RI_JINJO_YELLOW_BUBBLEGLOOP_SWAMP && randoItemId <= RI_JINJO_YELLOW_TREASURE_TROVE_COVE) {
-        collectibleInfo.modelId = ASSET_3BB_MODEL_JINJO_YELLOW;
-        collectibleInfo.draw_func = actor_draw;
-    }
-
-    // Molehill
-    else if (randoItemId >= RI_MOLEHILL_BARGE && randoItemId <= RI_MOLEHILL_WONDERWING) {
-        collectibleInfo.modelId = ASSET_388_MODEL_MOLEHILL;
-        collectibleInfo.draw_func = actor_draw;
-    }
-
-    // Mumbo Token
-    else if (randoItemId >= RI_MUMBO_TOKEN_BUBBLEGLOOP_SWAMP && randoItemId <= RI_MUMBO_TOKEN_TREASURE_TROVE_COVE) {
-        collectibleInfo.modelId = ASSET_41A_SPRITE_MUMBO_TOKEN;
-        collectibleInfo.draw_func = fxTouchSparkle_draw;
-    }
-
-    // Music Note
-    else if (randoItemId >= RI_MUSIC_NOTE_BUBBLEGLOOP_SWAMP && randoItemId <= RI_MUSIC_NOTE_TREASURE_TROVE_COVE) {
-        collectibleInfo.modelId = ASSET_6D6_SPRITE_MUSIC_NOTE;
-        collectibleInfo.draw_func = fxTouchSparkle_draw;
-    }
-
-    // StopNSwop Egg (Blue)
-    else if (randoItemId == RI_STOP_N_SWOP_EGG_BLUE) {
-        collectibleInfo.modelId = ASSET_36D_SPRITE_BLUE_EGG;
-        collectibleInfo.draw_func = fxTouchSparkle_draw;
-    }
-
-    // StopNSwop Egg (Cyan)
-    else if (randoItemId == RI_STOP_N_SWOP_EGG_CYAN) {
-        collectibleInfo.modelId = ASSET_36D_SPRITE_BLUE_EGG;
-        collectibleInfo.draw_func = fxTouchSparkle_draw;
-    }
-
-    // StopNSwop Egg (Green)
-    else if (randoItemId == RI_STOP_N_SWOP_EGG_GREEN) {
-        collectibleInfo.modelId = ASSET_36D_SPRITE_BLUE_EGG;
-        collectibleInfo.draw_func = fxTouchSparkle_draw;
-    }
-
-    // StopNSwop Egg (Pink)
-    else if (randoItemId == RI_STOP_N_SWOP_EGG_PINK) {
-        collectibleInfo.modelId = ASSET_36D_SPRITE_BLUE_EGG;
-        collectibleInfo.draw_func = fxTouchSparkle_draw;
-    }
-
-    // StopNSwop Egg (Red)
-    else if (randoItemId == RI_STOP_N_SWOP_EGG_RED) {
-        collectibleInfo.modelId = ASSET_36D_SPRITE_BLUE_EGG;
-        collectibleInfo.draw_func = fxTouchSparkle_draw;
-    }
-
-    // StopNSwop Egg (Yellow)
-    else if (randoItemId == RI_STOP_N_SWOP_EGG_YELLOW) {
-        collectibleInfo.modelId = ASSET_36D_SPRITE_BLUE_EGG;
-        collectibleInfo.draw_func = fxTouchSparkle_draw;
-    }
-
-    // StopNSwop Ice Key
-    else if (randoItemId == RI_STOP_N_SWOP_ICE_KEY) {
-        collectibleInfo.modelId = ASSET_36D_SPRITE_BLUE_EGG;
-        collectibleInfo.draw_func = fxTouchSparkle_draw;
+    switch (drawInfo->second.drawType) { 
+        case CCT_VANILLA_MODEL:
+            collectibleInfo.draw_func = actor_draw;
+            break;
+        case CCT_VANILLA_SPRITE:
+            collectibleInfo.draw_func = fxTouchSparkle_draw;
+            break;
+        case CCT_VANILLA_SNS_EGG:
+            collectibleInfo.draw_func = func_802D41C4;
+            break;
+        case CCT_CUSTOM_MODEL:
+            collectibleInfo.draw_func = CustomCollectible_DrawCustomModel;
+            break;
+        default:
+            break;
     }
 
     return collectibleInfo;
--- src/port/Rando/CustomCollectible/CustomCollectible.h
+++ src/port/Rando/CustomCollectible/CustomCollectible.h
@@ -3,8 +3,15 @@
 #include "port/Rando/Types.h"
 
 typedef struct {
-    uint32_t randoItemId;
+    uint16_t randoItemId;
 } ActorLocal_CustomCollectible;
+
+enum CustomCollectibleDrawTypes { CCT_VANILLA_MODEL, CCT_VANILLA_SPRITE, CCT_VANILLA_SNS_EGG, CCT_CUSTOM_MODEL };
+
+typedef struct {
+    asset_e drawModel;
+    CustomCollectibleDrawTypes drawType;
+} CustomCollectibleDrawInfo;
 
 void CustomCollectible_Update(Actor* actor);
 
@@ -13,6 +20,7 @@ private:
     static Actor* AttachCustomVariables(int32_t randoItemId, Actor* customCollectible);
     static void OnCollect(struct actorMarker_s* self, struct actorMarker_s* other);
     static ActorInfo GetActorAndDrawInfo(RandoItemId randoItemId);
+    static uint32_t GetSNSEggColor(RandoItemId randoItemId);
 
 public:
     static Actor* Spawn(int32_t position[3], RandoItemId randoItemId);
